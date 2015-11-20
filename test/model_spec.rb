a3e0a6ac4286355f60f469a63568d183e0623574
Jay Quigley <quigley.james.g@gmail.com>
2015-11-20 12:31:03 -0600

    minor syntax refactor for readability

--- test/model_spec.rb
+++ test/model_spec.rb
@@ -6,41 +6,41 @@ describe "Game" do
 
 	describe "#find_available_spaces" do
 		it "should find the available spaces" do
-			g.board = ["X", "X", "X", "3", "4", "5", "6", "7", "8"]
+			g.board = %w[X X X 3 4 5 6 7 8]
 			expect(g.find_available_spaces(g.board)).to eq(%w[3 4 5 6 7 8])
 		end
 	end
 
 	describe "#generate_computer_move" do
 		it "should choose the center if the center is open (on the first move)" do
-			g.board = ["O", "1", "2", "3", "4", "5", "6", "7", "8"]
+			g.board = %w[O 1 2 3 4 5 6 7 8]
 			expect(g.generate_computer_move).to eq(4)
 		end
 	end
 
 	describe "#get_best_move" do
 		it "should win if possible (above)" do
-			g.board = ["0", "1", "2", "X", "4", "5", "X", "7", "8"]
+			g.board = %w[0 1 2 X 4 5 X 7 8]
 			expect(g.get_best_move(g.board)).to eq(0)
 		end
 		it "should win if possible (interior)" do
-			g.board = ["0", "1", "X", "3", "4", "5", "6", "7", "X"]
+			g.board = %w[0 1 X 3 4 5 6 7 X]
 			expect(g.get_best_move(g.board)).to eq(5)
 		end
 		it "should win if possible (below)" do
-			g.board = ["0", "X", "2", "3", "X", "5", "6", "7", "8"]
+			g.board = %w[0 X 2 3 X 5 6 7 8]
 			expect(g.get_best_move(g.board)).to eq(7)
 		end
 		it "should block human (above)" do
-			g.board = ["0", "1", "2", "O", "4", "5", "O", "7", "8"]
+			g.board = %w[0 1 2 O 4 5 O 7 8]
 			expect(g.get_best_move(g.board)).to eq(0)
 		end
 		it "should block human (interior)" do
-			g.board = ["0", "1", "O", "3", "4", "5", "6", "7", "O"]
+			g.board = %w[0 1 O 3 4 5 6 7 O]
 			expect(g.get_best_move(g.board)).to eq(5)
 		end
 		it "should block human (below)" do
-			g.board = ["0", "O", "2", "3", "O", "5", "6", "7", "8"]
+			g.board = %w[0 O 2 3 O 5 6 7 8]
 			expect(g.get_best_move(g.board)).to eq(7)
 		end
 	end
@@ -64,22 +64,22 @@ describe "Game" do
 			expect(g.someone_won).to eq(false)
 		end		
 		it "should return true if someone has won" do
-			g.board = ["O", "O", "O", "3", "4", "5", "6", "7", "8"]
+			g.board = %w[O O O 3 4 5 6 7 8]
 			expect(g.someone_won).to eq(true)
 		end
 	end
 
 	describe "#set_winner" do
 		it "should set the human as the winner if the human has won" do
-			g.board = ["O", "O", "O", "3", "4", "5", "6", "7", "8"]
+			g.board = %w[O O O 3 4 5 6 7 8]
 			expect(g.set_winner).to eq(g.human_mark)
 		end
 		it "should set the computer as the winner if the computer has won" do
-			g.board = ["X", "X", "X", "3", "4", "5", "6", "7", "8"]
+			g.board = %w[X X X 3 4 5 6 7 8]
 			expect(g.set_winner).to eq(g.computer_mark)
 		end
 		it "should not set a winner even if the board is full" do
-			g.board = ["O", "X", "O", "X", "O", "O", "X", "O", "X"]
+			g.board = %w[O X O X O O X O X]
 			expect(g.set_winner).to eq(nil)
 		end
 	end
