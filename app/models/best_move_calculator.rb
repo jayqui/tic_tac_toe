67d26933beb12b2a1f77842b4b969190b122a33c
Jay Quigley <quigley.james.g@gmail.com>
2015-11-20 14:37:29 -0600

    add tests for wins and losses

--- app/models/best_move_calculator.rb
+++ app/models/best_move_calculator.rb
@@ -19,4 +19,29 @@ class BestMoveCalculator
 		end
 	end
 
+	def win?(piece)
+		combinations.any? { |comb| comb.uniq == [piece] }
+	end
+
+	def loss?(piece)
+		combinations.any? { |comb| comb.uniq == [other_piece(piece)] }
+	end
+
+	private
+
+	def other_piece(piece)
+		piece == computer_mark ? human_mark : computer_mark
+	end
+
+	def combinations
+	  [ [board[0], board[1], board[2]],
+	    [board[3], board[4], board[5]],
+	    [board[6], board[7], board[8]],
+	    [board[0], board[3], board[6]],
+	    [board[1], board[4], board[7]],
+	    [board[2], board[5], board[8]],
+	    [board[0], board[4], board[8]],
+	    [board[2], board[4], board[6]] ]
+	end
+
 end
--- test/best_move_calculator_spec.rb
+++ test/best_move_calculator_spec.rb
@@ -25,4 +25,30 @@ describe BestMoveCalculator do
 		end
 	end
 
+	describe "#win?" do
+		it "should correctly identify a win" do
+			bmc.board = %w[X X X O X O O 7 O]
+			expect(bmc.win?("X")).to eq(true)
+		end
+		it "should correctly identify a non-win" do
+			bmc.board = %w[X 1 X O X O O 7 O]
+			expect(bmc.win?("X")).to eq(false)
+		end
+	end
+
+	describe "#loss?" do
+		it "should correctly idenitfy a loss" do
+			bmc.board = %w[O O O 
+										 X 4 X
+										 X O X]
+			expect(bmc.loss?("X")).to eq(true)
+		end
+		it "should correctly idenitfy a non-loss" do
+			bmc.board = %w[O 1 O 
+										 X 4 X
+										 X O X]
+			expect(bmc.loss?("X")).to eq(false)
+		end
+	end
+
 end
